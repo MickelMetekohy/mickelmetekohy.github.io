ac8f663c35fb08042ef128cb063a72e3e04614ba
Mickel Metekohy <m.star28@gmail.com>
2019-05-22 18:50:17 +0200

    initial commit

---
+++ app.c0abb006e25d9addffda.hot-update.js
@@ -0,0 +1,21 @@
+webpackHotUpdate("app",{
+
+/***/ "./_src/js/app.js":
+/*!************************!*\
+  !*** ./_src/js/app.js ***!
+  \************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/app.scss */ "./_src/scss/app.scss");
+/* harmony import */ var _scss_app_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_scss_app_scss__WEBPACK_IMPORTED_MODULE_0__);
+
+
+(function closure() {})();
+
+/***/ })
+
+})
+//# sourceMappingURL=app.c0abb006e25d9addffda.hot-update.js.map
